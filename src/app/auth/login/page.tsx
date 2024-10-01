2e9859e9a8a17945e370be2e2f4301888663e9fb
Shishir19999 <shishirsharma199@gmail.com>
2024-10-01 09:27:27 +0545

    update

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='bg-white mon-w-[600px] min-h-[400px] p-[50px] rounded-[10px]'>
+<h1 className='text-center font-bold text-[30px] text-[#666]'>Login Page</h1>
+      <form>
+
+      </form>
+    </div>
+  )
+}
---
+++ src/app/auth/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+import {useRouter} from 'next/navigation'
+
+export default function AuthPage() {
+  const router=useRouter();
+    return (
+    router.push('auth/login')
+  )
+}

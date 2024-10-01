7c2c2120efb0832f592f3c4c508ecb9d27cc6b8f
Shishir19999 <shishirsharma199@gmail.com>
2024-10-01 11:48:25 +0545

    update

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,10 +1,18 @@
+"use client"
+import { usePathname } from 'next/navigation'
 import React,{ReactNode} from 'react'
 
 
 export default function layout({children}:{children:ReactNode}) {
+  usePathname()
   return (
-    <div>
+    <div className='flex justify-center h-screen bg-blue-[400]'>
+         <div className='bg-white mon-w-[600px] min-h-[400px] p-[50px] rounded-[10px]'>
+
+      <h1 className='bg-white mon-w-[600px] min-h-[400px] p-[50px] rounded-[10px]'></h1>
+
       {children}
+      </div>
     </div>
   )
 }
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import Link from 'next/link'
 
 export default function page() {
+  
+  const handleSubmit=(e:any)=>{
+    e.preventDefault();
+  }
   return (
-    <div className='bg-white mon-w-[600px] min-h-[400px] p-[50px] rounded-[10px]'>
-<h1 className='text-center font-bold text-[30px] text-[#666]'>Login Page</h1>
-      <form>
-
-      </form>
-    </div>
-  )
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="enter your email"
+        className="w-full border border-[#666] h-[60px] p-[15px] rounded-[50px] text-[18px] mb-[15px]"
+      />
+      <input
+        type="password"
+        placeholder="enter your password"
+        className="w-full border border-[#666] h-[60px] p-[15px] rounded-[50px] text-[18px] mb-[15px]"
+      />
+      <p className='text-[#666] text-[18px] '>Don't have an account ?
+      <Link href="/auth/register"> Register</Link>
+      </p>
+      <button type="submit" className=''>Sign In</button>
+    </form>
+  );
 }
